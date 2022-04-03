d32db1d1b73d0feab0bdb383e3778b43ebc1f775
naive1988 <1978253505@qq.com>
2022-04-03 20:52:42 +0800

    visit cloudfare worker

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function useFetch() {
+  const [state, setState] = useState('fetching message ...')
+
+  useEffect(() => {
+    fetch('https://my-app.naive1988.workers.dev/')
+      .then(res => res.json())
+      .then(
+        ({ data }) => { setState(data) },
+        () => {setState('fetch failed, please reload page')}
+      )
+  }, [])
+
+  return [state]
+}
+
 function App() {
+  const [message] = useFetch()
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>{message}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
